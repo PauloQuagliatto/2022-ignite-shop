89287168aad79ec11c74c40f92bba231d9c9a73b
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2022-11-28 11:00:15 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { styled } from '@stitches/react'
+
+const Button = styled("button", {
+  backgroundColor: '$rocketseat'
+})
+
+export default function Home() {
+  return (
+    <h1> a</h1>
+  )
+}
