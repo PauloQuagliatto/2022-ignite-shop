cba1f28ea72caa325998b570cf7af12622410ef2
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2022-11-29 10:56:12 -0300

    stripe added and pages styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,71 @@
-import { styled } from '@stitches/react'
+import { GetStaticProps } from 'next'
+import Link from 'next/link'
+import Image from 'next/image'
+import Stripe from 'stripe'
+import { useKeenSlider } from 'keen-slider/react'
 
-const Button = styled("button", {
-  backgroundColor: '$rocketseat'
-})
+import { stripe } from '../lib/stripe'
 
-export default function Home() {
+import { HomeContainer, ProductCard } from '../styles/pages/home'
+
+import 'keen-slider/keen-slider.min.css'
+
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
   return (
-    <h1> a</h1>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map((product) => (
+        <Link key={product.id} href={`/product/${product.id}`}>
+          <ProductCard className='keen-slider__slide'>
+            <Image src={product.imageUrl} width={520} height={480} alt='' />
+
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </ProductCard>
+        </Link>
+      ))}
+    </HomeContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount! / 100)
+    }
+  })
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 1
+  }
 }
---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,61 @@
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
+import Stripe from 'stripe'
+
+import { stripe } from '../../lib/stripe'
+
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
+
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt='' />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params!.id
+  const response = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = response.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: response.id,
+        name: response.name,
+        imageUrl: response.images[0],
+        price: new Intl.NumberFormat('pt-br', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount! / 100),
+        description: response.description
+      }
+    },
+    revalidate: 60 * 60 * 1
+  }
+}
